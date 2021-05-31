f4b74d1fc0a12d67599c5c5d69dfd060200342bf
jiaojinxing <jiaojinxing@acoinfo.com>
2021-05-31 11:41:24 +0800

    Updated sddc example print info.

--- examples/protocols/sddc/main/sddc_esp8266_example.c
+++ examples/protocols/sddc/main/sddc_esp8266_example.c
@@ -237,45 +237,45 @@ static void initialise_wifi(void)
 static void sc_callback(smartconfig_status_t status, void *pdata)
 {
     switch (status) {
-        case SC_STATUS_WAIT:
-            ESP_LOGI(TAG, "SC_STATUS_WAIT");
-            break;
-        case SC_STATUS_FIND_CHANNEL:
-            ESP_LOGI(TAG, "SC_STATUS_FINDING_CHANNEL");
-            break;
-        case SC_STATUS_GETTING_SSID_PSWD:
-            ESP_LOGI(TAG, "SC_STATUS_GETTING_SSID_PSWD");
-            break;
-        case SC_STATUS_LINK:
-            ESP_LOGI(TAG, "SC_STATUS_LINK");
-            wifi_config_t *wifi_config = pdata;
-            ESP_LOGI(TAG, "SSID:%s", wifi_config->sta.ssid);
-            ESP_LOGI(TAG, "PASSWORD:%s", wifi_config->sta.password);
-            ESP_ERROR_CHECK( esp_wifi_disconnect() );
-            ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, wifi_config) );
-            ESP_ERROR_CHECK( esp_wifi_connect() );
-            break;
-        case SC_STATUS_LINK_OVER:
-            ESP_LOGI(TAG, "SC_STATUS_LINK_OVER");
-            if (pdata != NULL) {
-                sc_callback_data_t *sc_callback_data = (sc_callback_data_t *)pdata;
-                switch (sc_callback_data->type) {
-                    case SC_ACK_TYPE_ESPTOUCH:
-                        ESP_LOGI(TAG, "Phone ip: %d.%d.%d.%d", sc_callback_data->ip[0], sc_callback_data->ip[1], sc_callback_data->ip[2], sc_callback_data->ip[3]);
-                        ESP_LOGI(TAG, "TYPE: ESPTOUCH");
-                        break;
-                    case SC_ACK_TYPE_AIRKISS:
-                        ESP_LOGI(TAG, "TYPE: AIRKISS");
-                        break;
-                    default:
-                        ESP_LOGE(TAG, "TYPE: ERROR");
-                        break;
-                }
+    case SC_STATUS_WAIT:
+        ESP_LOGI(TAG, "SC_STATUS_WAIT");
+        break;
+    case SC_STATUS_FIND_CHANNEL:
+        ESP_LOGI(TAG, "SC_STATUS_FIND_CHANNEL");
+        break;
+    case SC_STATUS_GETTING_SSID_PSWD:
+        ESP_LOGI(TAG, "SC_STATUS_GETTING_SSID_PSWD");
+        break;
+    case SC_STATUS_LINK:
+        ESP_LOGI(TAG, "SC_STATUS_LINK");
+        wifi_config_t *wifi_config = pdata;
+        ESP_LOGI(TAG, "SSID:%s", wifi_config->sta.ssid);
+        ESP_LOGI(TAG, "PASSWORD:%s", wifi_config->sta.password);
+        ESP_ERROR_CHECK( esp_wifi_disconnect() );
+        ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, wifi_config) );
+        ESP_ERROR_CHECK( esp_wifi_connect() );
+        break;
+    case SC_STATUS_LINK_OVER:
+        ESP_LOGI(TAG, "SC_STATUS_LINK_OVER");
+        if (pdata != NULL) {
+            sc_callback_data_t *sc_callback_data = (sc_callback_data_t *)pdata;
+            switch (sc_callback_data->type) {
+            case SC_ACK_TYPE_ESPTOUCH:
+                ESP_LOGI(TAG, "Phone IP: %d.%d.%d.%d", sc_callback_data->ip[0], sc_callback_data->ip[1], sc_callback_data->ip[2], sc_callback_data->ip[3]);
+                ESP_LOGI(TAG, "TYPE: ESPTOUCH");
+                break;
+            case SC_ACK_TYPE_AIRKISS:
+                ESP_LOGI(TAG, "TYPE: AIRKISS");
+                break;
+            default:
+                ESP_LOGE(TAG, "TYPE: ERROR");
+                break;
             }
-            xEventGroupSetBits(wifi_event_group, ESPTOUCH_DONE_BIT);
-            break;
-        default:
-            break;
+        }
+        xEventGroupSetBits(wifi_event_group, ESPTOUCH_DONE_BIT);
+        break;
+    default:
+        break;
     }
 }
 
@@ -287,10 +287,10 @@ static void smartconfig_task(void * parm)
     while (1) {
         uxBits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
         if(uxBits & CONNECTED_BIT) {
-            ESP_LOGI(TAG, "WiFi Connected to ap");
+            ESP_LOGI(TAG, "WiFi Connected to AP");
         }
         if(uxBits & ESPTOUCH_DONE_BIT) {
-            ESP_LOGI(TAG, "smartconfig over");
+            ESP_LOGI(TAG, "SmartConfig over");
             esp_smartconfig_stop();
             vTaskDelete(NULL);
             start_wifi_flag = 0;
@@ -310,7 +310,7 @@ static void flash_key_task(void *arg)
         }
         if (i > 3) {
             i = 0;
-            sddc_printf("start smart config....\n");
+            sddc_printf("Start SmartConfig....\n");
             if (!start_wifi_flag) {
                 initialise_wifi();
                 xTaskCreate(smartconfig_task, "smartconfig_task", 2048, NULL, 10, NULL);
